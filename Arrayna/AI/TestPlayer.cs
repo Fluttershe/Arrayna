ad6aa700652ab02fa79ec42d4690cdf01344be61
WenNan4 <xyzwennanxyz@qq.com>
2018-01-02 00:52:12 +0800

    用特别智障的方法完成了一个特别智障的AI= =

---
+++ Arrayna/AI/TestPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayer : MonoBehaviour
+{
+    public int speed;
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector2(0, 1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector2(0, -1) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector2(-1, 0) * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector2(1, 0) * speed * Time.deltaTime);
+        }
+    }
+}
